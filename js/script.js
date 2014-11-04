ac53e343d22969899588337eacf72970a570251a
jackielu <jackielu@gmail.com>
2014-11-04 12:56:13 -0500

    added pics

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 var buttonid;
 var isHeThere;
 var mID;
+var picID;
 
-var map = L.map('map').setView([40.7305404,-73.949101], 13); //adds a new map to the page, 'map' refers to the name of your div
+var map = L.map('map').setView([40.7305404,-73.949101], 12); //adds a new map to the page, 'map' refers to the name of your div
 //you define the variable map so that you can refer to it later
 //setview tells you where to start the map, after the comma comes the zoom level
 
@@ -24,49 +25,49 @@ L.tileLayer('http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.png', {
 
 //define the location markers, along with a title, and a function called onClick that runs when you click on the marker
 
-var bday = L.marker([41.077333,-73.501891], {icon: myIcon, title: 'bday', alt: 'where Seymour was born and his littermates still live'})
+var bday = L.marker([41.077333,-73.501891], {icon: myIcon, title: 'bday', alt: 'where Seymour was born and his littermates still live', pic: '/images/littermates.jpg'})
 	.addTo(map)
 	.on('click',onClick)
 	.on('mouseover',onMouseOver)
 	.on('mouseout',onMouseOut);
 
-var mkt = L.marker([40.720042,-73.952907], {icon: myIcon, title: 'mkt', alt: 'a place full of yummy food smells'})
+var mkt = L.marker([40.720042,-73.952907], {icon: myIcon, title: 'mkt', alt: 'a place full of yummy food smells', pic: '/images/cobble.jpg'})
 	.addTo(map)
 	.on('click',onClick)
 	.on('mouseover',onMouseOver)
 	.on('mouseout',onMouseOut);
 
-var soccer = L.marker([40.724444,-73.943358], {icon: myIcon, title: 'soccer', alt: 'nice grass fields for chasing the ball'})
+var soccer = L.marker([40.724444,-73.943358], {icon: myIcon, title: 'soccer', alt: 'nice grass fields for chasing the ball', pic: '/images/soccer.jpg'})
 	.addTo(map)
 	.on('click',onClick)
 	.on('mouseover',onMouseOver)
 	.on('mouseout',onMouseOut);
 
-var beach = L.marker([40.56321,-73.883314], {icon: myIcon, title: 'beach', alt: 'fun with seagulls and waves'})
+var beach = L.marker([40.56321,-73.883314], {icon: myIcon, title: 'beach', alt: 'fun with seagulls and waves', pic: '/images/beach.jpg'})
 	.addTo(map)
 	.on('click',onClick)
 	.on('mouseover',onMouseOver)
 	.on('mouseout',onMouseOut);
 
-var meetup = L.marker([40.6699957,-73.9702993], {icon: myIcon, title: 'meetup', alt: 'where seymour plays with his fellow frenchies'})
+var meetup = L.marker([40.6699957,-73.9702993], {icon: myIcon, title: 'meetup', alt: 'where seymour plays with his fellow frenchies', pic: '/images/meetup.jpg'})
 	.addTo(map)
 	.on('click',onClick)
 	.on('mouseover',onMouseOver)
 	.on('mouseout',onMouseOut);
 
-var camping = L.marker([40.200983,-74.300908], {icon: myIcon, title: 'camping', alt: 'this is where seymour went camping for the first time'})
+var camping = L.marker([40.200983,-74.300908], {icon: myIcon, title: 'camping', alt: 'this is where seymour went camping for the first time', pic: '/images/turkeyswampcamp.jpg'})
 	.addTo(map)
 	.on('click',onClick)
 	.on('mouseover',onMouseOver)
 	.on('mouseout',onMouseOut);
 
-var fort = L.marker([40.794196,-73.77565], {icon: myIcon, title: 'fort', alt: 'dog-friendly field station where jackie brings seymour to work'})
+var fort = L.marker([40.794196,-73.77565], {icon: myIcon, title: 'fort', alt: 'dog-friendly field station where jackie brings seymour to work', pic: '/images/fort2.jpg'})
 	.addTo(map)
 	.on('click',onClick)
 	.on('mouseover',onMouseOver)
 	.on('mouseout',onMouseOut);
 
-var licH2o = L.marker([40.7430715,-73.9600146], {icon: myIcon, title: 'licH2o', alt: 'a dog run with a view of Manhattan!'})
+var licH2o = L.marker([40.7430715,-73.9600146], {icon: myIcon, title: 'licH2o', alt: 'a dog run with a view of Manhattan!', pic: '/images/picnictable2.jpg'})
 	.addTo(map)
 	.on('click',onClick)
 	.on('mouseover',onMouseOver)
@@ -75,11 +76,15 @@ var licH2o = L.marker([40.7430715,-73.9600146], {icon: myIcon, title: 'licH2o', 
 
 //defines what happens on marker clicks
 function onClick(e) {
+	//console.log(this.options.pic);
+	//console.log('url('.concat(this.options.pic).concat(')'));
+	var picID = 'url('.concat(this.options.pic).concat(')');
 	//console.log(this.options.title);  //return the title of the marker when it is clicked on 
 	var isHeThere = Math.floor((Math.random() * 100) + 1);  //random number pull
 	//console.log(isHeThere);
 	if (isHeThere < 35) {
-		$('.jackpot').fadeIn(250);}
+		$('.jackpot').fadeIn(250);
+		$('#image').css('background-image',picID).fadeIn(250);}
 		else {
 			$('.tryAgain').fadeIn(250);
 		}
@@ -102,6 +107,7 @@ function onMouseOut(e) {
 	$(mID).toggleClass('hover');
 	$('.tryAgain').fadeOut(250);
 	$('.jackpot').fadeOut(250);
+	$('#image').fadeOut(250);
 }
 
 
